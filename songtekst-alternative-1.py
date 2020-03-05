08fffefd1f55a8b009eb8611cea3441d25d3ec78
nerdicwalker <nerdicwalker@gmail.com>
2020-03-05 22:55:03 +0100

    songtekst-alternative-1.py

---
+++ songtekst-alternative-1.py
@@ -0,0 +1,11 @@
+songtext = {
+    'artist': "Genesis",
+    'song_title': "Invisible Touch",
+    'refrain': "She seems to have an invisible touch, yeah She reaches in, and grabs right hold of your heart She seems to have an invisible touch, yeah It takes control and slowly tears you apart",
+    'stanza_one': "Well I've been waiting, waiting here so long But thinking nothing, nothing could go wrong, ooh now I know She has a built in ability To take everything she sees And now it seems I'm falling, falling for her",
+    'stanza_two': "Well I don't really know her, I only know her name But she crawls under your skin You're never quite the same, and now I know She's got something you just can't trust And it's something mysterious And now it seems I'm falling, falling for her", 
+    'stanza_three': "Well she don't like losing, to her it's still a game And though she will mess up your life You'll want her just the same, and now I know She has a built in ability To take everything she sees And now it seems I've fallen, fallen for her",
+    'stanza_four': "She seems to have an invisible touch, yeah She seems to have an invisible touch, oh She seems to have an invisible touch, yeah She seems to have an invisible touch, oh She seems to have an invisible touch, yeah She seems to have an invisible touch, oh She seems to have an invisible touch, yeah She seems to have an invisible touch, oh"
+}
+
+print(f"Artiest: {songtext['artist']} Title: {songtext['song_title']}\n\n{songtext['stanza_one']}\n\n{songtext['refrain']}\n\n{songtext['stanza_two']}\n\n{songtext['refrain']}\n\n{songtext['refrain']}\n\n{songtext['stanza_three']}\n\n{songtext['refrain']}\n\n{songtext['stanza_four']}")
