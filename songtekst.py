3deeb459c43bc43cfb6bb0706b438a475bba7208
nerdicwalker <nerdicwalker@gmail.com>
2020-03-05 22:30:32 +0100

    songtekst.py

---
+++ songtekst.py
@@ -0,0 +1,69 @@
+# songtekst (source: https://www.lyrics.com/lyric/32089710/Genesis/Invisible+Touch)
+
+def refrain():
+    ""
+    print("""
+She seems to have an invisible touch, yeah
+She reaches in, and grabs right hold of your heart
+She seems to have an invisible touch, yeah
+It takes control and slowly tears you apart
+    """)
+
+def stanza_one():
+    ""
+    print("""
+Well I've been waiting, waiting here so long
+But thinking nothing, nothing could go wrong, ooh now I know
+She has a built in ability
+To take everything she sees
+And now it seems I'm falling, falling for her
+    """)
+
+def stanza_two():
+    ""
+    print("""
+Well I don't really know her, I only know her name
+But she crawls under your skin
+You're never quite the same, and now I know
+She's got something you just can't trust
+And it's something mysterious
+And now it seems I'm falling, falling for her
+    """)
+
+def stanza_three():
+    ""
+    print("""
+Well she don't like losing, to her it's still a game
+And though she will mess up your life
+You'll want her just the same, and now I know
+She has a built in ability
+To take everything she sees
+And now it seems I've fallen, fallen for her
+    """)
+
+def stanza_four():
+    ""
+    print(""" 
+She seems to have an invisible touch, yeah
+She seems to have an invisible touch, oh
+She seems to have an invisible touch, yeah
+She seems to have an invisible touch, oh
+She seems to have an invisible touch, yeah
+She seems to have an invisible touch, oh
+She seems to have an invisible touch, yeah
+She seems to have an invisible touch, oh
+    """)
+
+artist = "Genesis"
+song_title = "Invisible Touch"
+print(f"Artiest: {artist} nummer: {song_title}.\n")
+
+
+stanza_one()
+refrain()
+stanza_two()
+refrain()
+refrain()
+stanza_three()
+refrain()
+stanza_four()
--- week1-assignments.py
+++ week1-assignments.py
@@ -9,3 +9,77 @@ print(9*9)
 
 # machtsverheffen '^'
 print(9**9)
+
+
+# Stel we hebben een rechthoek met lengte 5 en breedte 3. 
+# Gebruik een operator om de oppervlakte te berekenen en 
+# druk het resultaat af op het scherm met print.
+
+length = 5
+width = 3
+surface_area = length * width
+print(f"De oppervlakte is: {surface_area}.")
+
+# Stel we hebben een pizza met 8 punten en we willen die 
+# verdelen onder 5 hardwerkende programmeurs. 
+# Hoeveel pizza punten krijgt elke programmeur? 
+# Gebruik een operator om het aantal pizza punten 
+# te berekenen en druk het resultaat af op het scherm met print.
+
+slices = 8 
+programmers = 5
+amount_of_pizza = slices / programmers
+print(f"Iedere hardwerkende programmeur krijg: {amount_of_pizza} pizza punt.")
+
+# Zo kunnen we bijvoorbeeld strings optellen / samenvoegen met de operator +.
+# Type: print("Hello" + "World")
+
+print("Hello" + "World") # output: HelloWorld
+
+# Dat is nog niet ideaal: er ontbreekt een spatie tussen Hello en World. 
+# Kun je het voorgaande commando aanpassen om wel een spatie tussen de 
+# twee woorden te krijgen, 
+# zonder de strings "Hello" en "World" zelf te veranderen?
+
+print("Hello" + " World") # output: Hello World
+
+# Een leuke truc is, om strings te vermenigvuldigen. 
+# Stel: je hebt 100 uitroeptekens nodig. 
+# Een echte programmeur is natuurlijk te lui om dat met de hand in te typen, 
+# en wil dat automatiseren!
+# Type: print(100 * "!")
+
+print(100 * "!") # '!' (100 times)
+
+# Strings beginnen en eindigen met quotes: "string". Dat is een probleem als er quotes
+# in de string zelf voorkomen.
+# 4. Probeer de volgende string te printen, inclusief de quotes om 
+# "Python is leuk": Guido zei: "Python is leuk".
+# print("Guido zei: "Python is leuk".") # SyntaxError: invalid syntax
+print('Guido zei: "Python is leuk".')
+
+# Hiermee is het probleem niet helemaal opgelost. 
+# Wat nu, als je een string wil afdrukken met zowel enkele als dubbele quotes er in? 
+# Dan kun je gebruik maken van een escape sequence door een backslash: \ voor een bijzonder 
+# karakter te zetten.
+
+print("Hello \nWorld!") # '\n' new line
+
+# Een string is eigenlijk een serie letters. Soms wil je één bepaalde letter uit een string 
+# gebruiken. Dat kan door achter de string het nummer van de gewenste letter in 
+# blokhaken: [ ] te zetten. De letters worden van links naar rechts geteld vanaf 0. Probeer maar:
+# print( "hallo"[0] ) geeft: h print( "hallo"[1] ) geeft: a
+# 1. Pas het onderstaande commando aan om te zorgen dat er een kleine letter e wordt geprint:
+
+print("Python4Ever"[9])
+
+# We kunnen de twee datatypes int en string naar elkaar omzetten met de functies: int en str. 
+# We hebben al eerder een functie gebruikt, namelijk print. Net als bij print geven we de 
+# benodigde informatie mee tussen haakjes: ( ). Probeer maar:
+# int( "216") geeft: 216 str( 216 ) geeft: '216'
+# We kunnen in principe van elke int een string maken, maar kun je ook van elke string een int maken?
+# 1. Bekijk de volgende twee commando’s 
+print( int("4") + int("2") )
+print( str(4) + str(2) )
+
+
