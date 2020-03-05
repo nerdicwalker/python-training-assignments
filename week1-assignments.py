861428ee4ba6fd3c333cc36754f1b228fc75a265
nerdicwalker <nerdicwalker@gmail.com>
2020-03-05 21:58:38 +0100

    .

---
+++ week1-assignments.py
@@ -0,0 +1,11 @@
+# Welke operator gebruiken we voor optellen? En welke voor vermenigvuldigen, delen en
+# machtsverheffen?
+
+# optellen '+'
+print(9+9)
+
+# vermenigvuldigen '*'
+print(9*9)
+
+# machtsverheffen '^'
+print(9**9)
